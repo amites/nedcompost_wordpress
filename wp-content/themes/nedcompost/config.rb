ec0a36328ed0a96e92a9a68e2f2c5587ca7ad519
Alvin Mites <alvin@mitesdesign.com>
2015-07-06 01:35:29 -0600

    added wordpress

---
+++ wp-content/themes/nedcompost/config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+
+
+# Set this to the root of your project when deployed:
+http_path = "/static/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "/static/images"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :compressed
+output_style = :compressed
+
+sass_options = {
+#    :debug_info => true,
+    :sourcemap => true
+}
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+
+preferred_syntax = :scss
